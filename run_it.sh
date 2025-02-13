11808ca5a01dd5612e792393df59d99c95789cb4
elio torquet <elio.torquet@etu.umontpellier.fr>
2025-02-13 19:32:02 +0100

    update cluster script

--- run_it.sh
+++ run_it.sh
@@ -3,5 +3,5 @@
 for i in $(seq 1 5)
 do
 	echo "parsing $i"
-	python3 src/iteration_stats.py -f 0.15 -d 400 -ec 20000 270000 -lc 3000 -s 1 -sa 1-10 -it $i -o results/iteration_stats/v2.0/P$i
+	srun python3 src/iteration_stats.py -f 0.15 -d 400 -ec 20000 270000 -lc 3000 -s 1 -sa 1-10 -it $i -o results/iteration_stats/v3.0/P$i
 done
