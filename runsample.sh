241e9bc5718f47573249e4a1c21f1e5ed03c0db4
elio <elio.torquet@proton.me>
2025-02-26 19:07:07 +0100

    add candidates

--- runsample.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 1 9);
-do
-	echo "running $i"
-	srun python3 src/sample_stats.py -f 0.05 -d 150 -ec 10000 270000 -lc 2000 -s 1 -sa $i -it "1-5" -o results/sample_stats/$i
-done	
